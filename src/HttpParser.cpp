17aed37b1caf85ff1f20d4fa1c5f85ce987bdb1b
Corentin <cf.fleuret@gmail.com>
2025-11-20 13:27:42 +0100

    Upload complete (petit pb avec la fin du fichier upladed)

--- src/HttpParser.cpp
+++ src/HttpParser.cpp
@@ -103,7 +103,7 @@ void HttpRequest::clear()
 	_chunked = false;
 }
 
-ParseStatus parseHttpMessage(const std::string &buffer, HttpRequest &req)
+ParseStatus parseHttpMessage(const std::string buffer, HttpRequest &req)
 {
 	std::istringstream iss(buffer);
 	std::string line;
@@ -138,51 +138,11 @@ ParseStatus parseHttpMessage(const std::string &buffer, HttpRequest &req)
 			val.erase(0, 1);
 		while (!val.empty() && (val[val.size() - 1] == '\r' || val[val.size() - 1] == ' '))
 			val.erase(val.size() - 1);
-
 		req.addHeader(key, val);
 		if (key == "Content-Length")
 			req.setContentLength(std::atoi(val.c_str()));
 		else if (key == "Transfer-Encoding" && val == "chunked")
 			req.setChunked(true);
 	}
-	return PARSE_OK;
-}
-
-ParseStatus parseHttpBody(const std::string &buffer, HttpRequest &req)
-{
-	if (req.isChunked())
-	{
-		std::istringstream iss(buffer);
-		std::string result;
-		std::string line;
-		size_t chunk_size = 0;
-		while (std::getline(iss, line))
-		{
-			std::stringstream ss;
-			ss << std::hex << line;
-			ss >> chunk_size;
-
-			if (chunk_size == 0)
-				break;
-
-			std::string chunk_data(chunk_size, '\0');
-			iss.read(&chunk_data[0], chunk_size);
-			result += chunk_data;
-			iss.ignore(2);
-		}
-		req.setBody(result);
-		req.setBodyComplete(true);
-		return PARSE_OK;
-	}
-	if (req.getContentLength() == 0)
-	{
-		req.setBody("");
-		req.setBodyComplete(true);
-		return PARSE_OK;
-	}
-	if (buffer.size() < req.getContentLength())
-		return PARSE_INCOMPLETE;
-	req.setBody(buffer.substr(0, req.getContentLength()));
-	req.setBodyComplete(true);
 	return PARSE_OK;
 }
--- src/poll.cpp
+++ src/poll.cpp
@@ -68,6 +68,7 @@ void	Poll::add_socket(int sockfd)
 	{
 		this->_clientsrv[connection] = this->_listensrv[sockfd];
 		fcntl(connection, F_SETFL, O_NONBLOCK);
+		this->_buffer[connection] = "";
 		pollfd newfd;
 		newfd.fd = connection;
 		newfd.events = POLLIN;
@@ -75,6 +76,28 @@ void	Poll::add_socket(int sockfd)
 		this->_pollrequest.push_back(newfd);
 	}
 }
+
+bool requestIsComplete(const std::string &buffer)
+{
+	size_t header_end = buffer.find("\r\n\r\n");
+	if (header_end == std::string::npos)
+		return false;
+	std::string headers = buffer.substr(0, header_end);
+	size_t pos = headers.find("Content-Length:");
+	if (pos != std::string::npos)
+	{
+		pos += strlen("Content-Length:");
+		while (pos < headers.size() && (headers[pos] == ' ' || headers[pos] == '\t'))
+			pos++;
+		int contentLength;
+		std::string str = headers.c_str() + pos;
+		std::istringstream(str) >> contentLength;
+		size_t total_needed = header_end + 4 + contentLength;
+		return buffer.size() >= total_needed;
+	}
+	return true;
+}
+
 
 int	Poll::send_socket(int i, const Server& srv)
 {
@@ -91,9 +114,14 @@ int	Poll::send_socket(int i, const Server& srv)
 	else
 	{
 		buffer[bytesRead] = '\0';
-		std::string response = handleClient(srv, buffer, this->_pollrequest);
+		this->_buffer[this->_pollrequest[i].fd] += buffer;
+		if (requestIsComplete(this->_buffer[this->_pollrequest[i].fd]))
+		{
+			std::string response = handleClient(srv, this->_buffer[this->_pollrequest[i].fd], this->_pollrequest);
+			send(this->_pollrequest[i].fd, response.c_str(), response.size(), 0);
+			this->_buffer.erase(this->_pollrequest[i].fd);
+		}
 		delete [] buffer;
-		send(this->_pollrequest[i].fd, response.c_str(), response.size(), 0);
 	}
 	return i;
 }
